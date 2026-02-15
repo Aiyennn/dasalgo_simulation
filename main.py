1339e3f6bdc5328c25109d784bc73aff5bf8ff61
Aiyennn <146877043+Aiyennn@users.noreply.github.com>
2026-02-15 14:15:13 +0800

    structure

--- main.py
+++ main.py
@@ -1,18 +1,101 @@
 def coin_change():
-    pass
+    print_title("Coin Change")
+
+    """
+    Coin Change code
+
+
+
+
+    """
 
 def huffman():
-    pass
+    print_title("Huffman")
+
+    
+    """
+    Huffman code
+
+
+
+    
+    """
 
 def fractional_knapsack():
-    pass
+    print_title("Fractional Knapsack")
+
+    
+    """
+    fractional knapsack code
+
+
+
+    
+    """
 
 def job_scheduling():
-    pass
+    print_title("Job Scheduling")
 
+    
+    """
+    job_scheduling code
+
+
+
+    
+    """
+
+def print_title(title: str):
+    width = 50
+    print("=" * width)
+    print(title.center(width))
+    print("=" * width)
+    print()
 
 def main():
-    pass
+
+    title = "GREEDY ALGORITHMS"
+    width = 50
+    algorithms = [
+        "Coin Change",
+        "Huffman Coding",
+        "Fractional Knapsack",
+        "Job Scheduling with Deadlines",
+        "Exit"
+    ]
+
+    while True:
+        print_title("Greedy Algorithms")
+
+        for i in range(0, len(algorithms)):
+            print(f"{i + 1}. {algorithms[i]}")
+        print()
+
+        isValid = False
+        while (not isValid):
+            try:
+                option = int(input("Select an option: "))
+                isValid = True
+            except ValueError:
+                print("Insert an integer")
+
+        match option:
+            case 1:
+                coin_change()
+            case 2:
+                huffman()
+            case 3:
+                fractional_knapsack()
+            case 4:
+                job_scheduling()
+            case 5:
+                break
+            case _:
+                print("Choose value between 1-5")
+
+        
+
+        
 
 if __name__ == "__main__":
     main()
