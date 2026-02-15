3c523c6bfd1259180cb31771c5b161909ade184b
Aiyennn <146877043+Aiyennn@users.noreply.github.com>
2026-02-15 13:55:28 +0800

    project_init

---
+++ main.py
@@ -0,0 +1,18 @@
+def coin_change():
+    pass
+
+def huffman():
+    pass
+
+def fractional_knapsack():
+    pass
+
+def job_scheduling():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
