0aebb92618aa9b992ac570880f8bc82ca0a8a9dd
Aiyennn <146877043+Aiyennn@users.noreply.github.com>
2026-02-15 20:04:38 +0800

    huffman

--- main.py
+++ main.py
@@ -1,3 +1,47 @@
+class Node:
+    def __init__(self, char=None, freq=0):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+# -------------------------------------------Classes must be declared above this line-----------------------------------------------------------
+
+
+# Huffman
+def create_tree(chars, freqs):
+    nodes = [Node(c, f) for c, f in zip(chars, freqs)]
+
+    while len(nodes) > 1:
+        nodes.sort(key=lambda x: x.freq)
+
+        left = nodes.pop(0)
+        right = nodes.pop(0)
+
+        merged = Node(freq=left.freq + right.freq)
+        merged.left = left
+        merged.right = right
+
+        nodes.append(merged)
+
+    return nodes[0]
+
+def generate_codes(node, prefix="", codes=None):
+    if codes is None:
+        codes = {}
+
+    if node.char is not None:
+        codes[node.char] = prefix
+        return codes
+
+    generate_codes(node.left, prefix + "0", codes)
+    generate_codes(node.right, prefix + "1", codes)
+
+    return codes
+# End of Huffman
+
+# -------------------------------------------Helper functions above this line-------------------------------------------------------------------
+
 def coin_change():
     print_title("Coin Change")
 
@@ -12,14 +56,24 @@ def coin_change():
 def huffman():
     print_title("Huffman")
 
-    
-    """
-    Huffman code
+    print_title("Enter values in Space Separated format: ")
+    print("Sample: ")
+    print("Enter Characters: A B C D")
+    print("Enter Frequencies: 10 2 3 5")
+    print("-------------------------------")
 
+    chars = input("Enter characters: ").split()
+    freqs = list(map(int, input("Enter frequencies: ").split()))
 
+    if len(chars) != len(freqs):
+        print("Number of Characters and Frequency doesn't match")
 
-    
-    """
+    root = create_tree(chars, freqs)
+    codes = generate_codes(root)
+
+    print("Huffman Codes:")
+    for ch in chars:
+        print(f"{ch}: {codes[ch]}")
 
 def fractional_knapsack():
     print_title("Fractional Knapsack")
